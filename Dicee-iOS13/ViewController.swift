587b357f54922e5175dc32c774432070a54ba8db
ohelwani <ohelwani@hemav.com>
2020-04-11 13:11:28 +0200

    46. How to Randomise the Dice Images: 	Adding Random Selection of Images and cleaning code.

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -14,15 +14,13 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var diceImageView2: UIImageView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
-    }
-
+    let arrayDiceImages = [#imageLiteral(resourceName: "DiceOne") ,#imageLiteral(resourceName: "DiceTwo") ,#imageLiteral(resourceName: "DiceThree") ,#imageLiteral(resourceName: "DiceFour") ,#imageLiteral(resourceName: "DiceFive") ,#imageLiteral(resourceName: "DiceSix")]
+    
     @IBAction func rollDiceButton(_ sender: UIButton) {
-        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+        
+        diceImageView1.image = arrayDiceImages.randomElement()
+        diceImageView2.image = arrayDiceImages.randomElement()
+        
     }
     
 }
