7e398c13451fc2899576ccf6c8fbaa79e8f6bb9f
ohelwani <ohelwani@hemav.com>
2020-04-11 11:02:21 +0200

    39. Responding to User Interactions with IBActions: 	Adding IBAction so, when the button is pressed both dices show the number 4

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -17,11 +17,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageView1.alpha = 0.5
-        
         diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
     }
 
-
+    @IBAction func rollDiceButton(_ sender: UIButton) {
+        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+    }
+    
 }
 
